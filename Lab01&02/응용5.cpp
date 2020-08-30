7814d5b66dde2c6c2bbf97555a273fe2aec9cc59
Ji yeon Lee <55075257+ppjy0927@users.noreply.github.com>
2020-08-30 23:27:14 +0900

    Add files via upload

---
+++ Lab01&02/응용5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float x, y;
+	cout << "Please enter Fahrenheit value: ";
+	cin >> x;
+	y = 5.0 / 9.0 * (x - 32);
+	cout << "Celsius value is " << y << endl;
+}
