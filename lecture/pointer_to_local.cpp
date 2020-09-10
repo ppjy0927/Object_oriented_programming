710278818771a66d4ec9848ea0acd81829f917e6
Ji yeon Lee <55075257+ppjy0927@users.noreply.github.com>
2020-09-10 22:34:53 +0900

    Add files via upload

---
+++ lecture/pointer_to_local.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int* func() {
+	int a = 100;
+	int* pa = &a;
+
+	cout << pa << endl;
+	cout << "*pa값은 " << *pa << " 입니다." <<  endl;
+	return pa;
+}
+
+int main() {
+	int* rpa = func(); // a의 주소값이었던 주소값이 저장되지만 같은 값을 가지진 않음.
+	// 함수가 종료되면서 a의 메모리가 할당해제되어 값이 없어지기 때문(a가 지역 변수이기 때문)
+
+	cout << rpa << endl;
+	cout << "리턴 값 *pa는 " << *rpa << " 입니다." << endl;
+}
