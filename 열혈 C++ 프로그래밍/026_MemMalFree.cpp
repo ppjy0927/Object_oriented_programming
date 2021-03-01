8bb816a3a86d6ae387aef0bb197a9be380729b38
Ji yeon Lee <55075257+ppjy0927@users.noreply.github.com>
2021-03-01 23:33:16 +0900

    Add files via upload

---
+++ 열혈 C++ 프로그래밍/026_MemMalFree.cpp
@@ -0,0 +1,20 @@
+// 02-5 : malloc & free를 대신하는 new & delete
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+
+char* MakeStrAdr(int len) {
+	char* str = (char*)malloc(sizeof(char)*len);
+	return str;
+}
+
+int main(void) {
+	char* str = MakeStrAdr(20);
+	strcpy(str, "I am so happy~");
+	cout << str << endl;
+	free(str);
+
+	return 0;
+}
