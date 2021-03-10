05c9fc944b3b06bee46c18d690429886e75450d3
Ji yeon Lee <55075257+ppjy0927@users.noreply.github.com>
2021-03-10 22:20:59 +0900

    Add files via upload

---
+++ 열혈 C++ 프로그래밍/Quiz03-1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct Point {
+	int xpos;
+	int ypos;
+
+	void MovePos(int x, int y) {
+		xpos += x;
+		ypos += y;
+	}
+
+	void ShowPosition() {
+		cout << "[" << xpos << ", " << ypos << "]" << endl;
+	}
+
+	void AddPoint(const Point &p) {
+		xpos += p.xpos;
+		ypos += p.ypos;
+	}
+};
+
+int main(void) {
+	Point pos1 = { 12, 4 };
+	Point pos2 = { 20, 30 };
+
+	pos1.MovePos(-7, 10);
+	pos1.ShowPosition(); // [5, 14] 출력
+
+	pos1.AddPoint(pos2);
+	pos1.ShowPosition(); // [25, 44] 출력
+
+	return 0;
+}
